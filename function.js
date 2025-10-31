48dc44ec177c437c4ba425854a2a449a3e6ae6b4
arul-collab <arulk9956@gmail.com>
2025-10-31 13:23:02 +0530

    sample

---
+++ function.js
@@ -0,0 +1,8 @@
+var a
+var b
+function add(){
+    a=5
+    b=10
+    console.log(a+b)
+}
+add() 
---
+++ practise.js
@@ -0,0 +1,5 @@
+var price=10
+var product=("book")
+var tax=2
+console.log(product)
+console.log(price+tax)
